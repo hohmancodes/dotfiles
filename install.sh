0dcfd9dea26737d5cceaf3427f59c8a29e6920ff
Matt Hohman <matt@hohman.codes>
2022-11-03 09:01:53 -0700

    Adding dotfiles template

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo ()
+{
+    [[ $EUID = 0 ]] || set -- command sudo "$@"
+    "$@"
+}
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+[ -f "$SCRIPT_DIR/install.log" ] && { echo >&2 "Dotfiles have already installed. Exiting!"; exit 1; }
+
+echo "install.sh ran at $(date) from $SCRIPT_DIR" >> $SCRIPT_DIR/install.log
+
+sudo $SCRIPT_DIR/system_install.sh
+$SCRIPT_DIR/user_install.sh
